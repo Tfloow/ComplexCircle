92c6726f25249df621a23df57318afda3535d3eb
Tfloow <88899420+Tfloow@users.noreply.github.com>
2021-09-25 21:55:21 +0200

    Add files via upload

--- repComplexCircle.py
+++ repComplexCircle.py
@@ -1,11 +1,17 @@
 from turtle import *
 import time
+import cmath
+import math
 
 point_of_interest=[]
-coordinatex = int(textinput("x", "Where do you want me to go on x?"))
-point_of_interest.append(coordinatex)
-coordinatey = int(textinput("y", "Where do you want me to go on y?"))
-point_of_interest.append(coordinatey)
+cmath.polar(math.sqrt(5) + 2j)
+coordinatex = int(textinput("x", "what is your real value"))
+coordinatey = int(textinput("y", "what is your complex value"))
+complexNumber = coordinatex + coordinatey*1j
+print(complexNumber)
+polarNumber = cmath.polar(complexNumber)
+point_of_interest.append(polarNumber[0])
+point_of_interest.append(polarNumber[1])
 print(point_of_interest)
 
 speed(0.4)
@@ -22,6 +28,10 @@ left(90)
 circle(100)
 
 penup()
-goto(point_of_interest[0], point_of_interest[1])
+goto(math.ceil(point_of_interest[0])*100, math.ceil(point_of_interest[1])*100)
+print(point_of_interest)
+print(pos())
 dot(10, "red")
+pendown()
+goto(100,0)
 exitonclick()
